9dff2310f8ce932a94034277dc679eeb35a18fd3
Linda L Sato <lsato@lib-ads0001.uoregon.edu>
2017-06-28 15:06:44 -0700

    use env var

--- app/services/oregon_digital_builder.rb
+++ app/services/oregon_digital_builder.rb
@@ -1,20 +1,31 @@
 require 'open-uri'
 class OregonDigitalBuilder < Spotlight::SolrDocumentBuilder
   def to_solr
-    content = JSON.parse(open(resource.url).read)
-    doc = content['response']['document']
-
+    doc = get_data(resource.url)
+    if !doc.nil?
       {
         id: doc['id'],
         full_title_tesim: doc['desc_metadata__title_tesim'],
-        Spotlight::Engine.config.thumbnail_field => "http://localhost:3000/downloads/#{doc['id']}.jpg",
-        oembed_url_ssm: "http://localhost:3000/resource/#{doc['id']}",
+        Spotlight::Engine.config.thumbnail_field => ENV['OD_URL'] + "/downloads/#{doc['id']}.jpg",
+        oembed_url_ssm: ENV['OD_URL'] + "/resource/#{doc['id']}",
         creator_ssim: extract_label(doc['desc_metadata__creator_label_ssm']),
         photographer_ssim: extract_label(doc['desc_metadata__photographer_label_ssm']),
         description_tesim: doc['desc_metadata__description_tesim'],
         subject_ssim: extract_label(doc['desc_metadata__lcsubject_label_ssm']),
         set_ssim: extract_label(doc['desc_metadata__set_label_ssm'])
-    }
+     }
+    else
+      nil
+    end
+  end
+
+  def get_data(url)
+    begin
+      content = JSON.parse(open(url).read)
+      content['response']['document']
+    rescue
+      nil
+    end
   end
 
   def extract_label(arr)
@@ -22,7 +33,9 @@ class OregonDigitalBuilder < Spotlight::SolrDocumentBuilder
     new_arr = []
     arr.each do |item|
       parts = item.split("$")
-      new_arr << parts[0]
+      if !parts[0].include? "http"
+        new_arr << parts[0]
+      end
     end
     new_arr
   end
--- config/initializers/oembed_providers.rb
+++ config/initializers/oembed_providers.rb
@@ -2,6 +2,6 @@ require 'oembed'
 
 OEmbed::Providers.register_all
 
-embed_provider = OEmbed::Provider.new("http://localhost:3000/oembed/?format=json")
-embed_provider << "http://localhost:3000/resource/*"
+embed_provider = OEmbed::Provider.new(ENV['OD_URL'] + "/oembed/?format=json")
+embed_provider << ENV['OD_URL'] + "/resource/*"
 OEmbed::Providers.register(embed_provider)
