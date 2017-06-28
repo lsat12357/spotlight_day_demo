7ee87f2da2b2b80b815a50cfba0a57f4ab4c7a74
Linda L Sato <lsato@lib-ads0001.uoregon.edu>
2017-06-28 15:08:07 -0700

    add validation

--- app/controllers/oregon_digital_resources_controller.rb
+++ app/controllers/oregon_digital_resources_controller.rb
@@ -5,21 +5,22 @@ class OregonDigitalResourcesController < ApplicationController
     if @resource.save_and_index
       redirect_to spotlight.admin_exhibit_catalog_path(current_exhibit)
     else
-      render 'edit'
+      flash[:notice] = @resource.errors[:base]
+      redirect_to spotlight.admin_exhibit_catalog_path(current_exhibit)
     end
   end
 
   private
 
-    def build_resource
-      @resource = begin
-                    r = OregonDigitalResource.new(resource_params)
-                    r.exhibit = current_exhibit
-                    r
-                  end
+  def build_resource
+    @resource = begin
+      r = OregonDigitalResource.new(resource_params)
+      r.exhibit = current_exhibit
+      r
     end
+  end
 
-    def resource_params
-      params.require(:oregon_digital_resource).permit(:url)
-    end
+  def resource_params
+    params.require(:oregon_digital_resource).permit(:url)
+  end
 end
--- app/models/oregon_digital_resource.rb
+++ app/models/oregon_digital_resource.rb
@@ -1,3 +1,4 @@
 class OregonDigitalResource < Spotlight::Resource
+  validates_with IsValidImportUrl
   self.document_builder_class = OregonDigitalBuilder
 end
